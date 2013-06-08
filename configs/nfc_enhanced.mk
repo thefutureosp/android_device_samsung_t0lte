2a7219067c95cad443abd263d671c11d9e078b39
slickrick <rsalinas1@mail.usf.edu>
2013-06-08 05:08:55 -0400

    Clean up
    Change-Id: Ie28d6394ab03d52a32b5a63df564fe228d3dbebb

---
+++ configs/nfc_enhanced.mk
@@ -0,0 +1,3 @@
+# This device supports CM enhanced NFC
+PRODUCT_COPY_FILES += \
+    device/samsung/t0lte/configs/com.cyanogenmod.nfc.enhanced.xml:system/etc/permissions/com.cyanogenmod.nfc.enhanced.xml
